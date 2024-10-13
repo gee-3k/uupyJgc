214d21aac05555a363c31a96851c6ddb3e7eac51
gee3k <gee3k.dev@gmail.com>
2024-10-13 20:58:21 +0700

    main.py - prints "Hello World !", then import task001.py as module and run it's start() function task001.py - def start() to implement (homework) task

---
+++ main.py
@@ -0,0 +1,19 @@
+"""
+urban university – python Junior grade course
+implemented by gee3k (c) 2024-2025
+"""
+
+"""
+Part "Module Zero"
+have registered GitHub
+installed and tuned: PyCharm, git, py
+
+IMHO, PyCharm is not good – has freezes sometime, i suppose it because of JavaVM
+VSCodium has more stable behavior for me, faster to edit text, and moving cursor across the code
+"""
+print( "Hello World !" )
+
+
+# Part to run next (homework) tasks
+import task001
+task001.start()
---
+++ task001.py
@@ -0,0 +1,28 @@
+"""
+urban university – python Junior grade course
+implemented by gee3k (c) 2024-2025
+"""
+
+def start():
+    print( "\n=* task001 *=" )
+
+    # 1st program
+    print( "1st program :", 9 ** 0.5 * 5 )
+
+    # 2nd program
+    print( "2nd program :", 9.99 > 9.98 and 1000 != 1001 )
+
+    # 3rd program
+    npr = 2 * 2 + 2
+    wpr = 2 * ( 2 + 2 )
+    print( "3rd program :", npr == wpr,
+           f"\n\t  no priority : { npr }",
+           f"\n\twith priority : { wpr }"
+           )
+
+    # 4th program
+    s = '123.456'
+    print( "4th program :",
+           f"\n\tby str.split : { s.split('.')[1][0] }",
+           f"\n\tby tech.spec.: { int( float(s) * 10 % 123 ) }"
+           )
