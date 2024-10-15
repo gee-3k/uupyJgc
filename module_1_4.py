11deb32eae3343c0577ab767831a18e1900f5aca
gee3k <gee3k.dev@gmail.com>
2024-10-16 02:12:49 +0700

    upd.

--- module_1_4.py
+++ module_1_4.py
@@ -13,11 +13,11 @@ def start():
     my_string = input( "please, type some/any text here and press an [enter] key when you've done\n>" )
     scnt = my_string.count('')-1
     print( f"You've just entered { scnt } chars long text",
-           f"\n\tmy_string .upper case : { my_string.upper() }",
-           f"\n\tmy_string .lower case : { my_string.lower() }",
-           f"\n\tmy_string .replace (del spaces): { my_string.replace( ' ', '' ) }",
-           f"\n\tmy_string 1st char : { my_string[0] }",
-           f"\n\tmy_string last char : { my_string[-1] }",
+           f"\n\tmy_string .upper case : '{ my_string.upper() }'",
+           f"\n\tmy_string .lower case : '{ my_string.lower() }'",
+           f"\n\tmy_string .replace (del spaces): '{ my_string.replace( ' ', '' ) }'",
+           f"\n\tmy_string 1st char : '{ my_string[0] }'",
+           f"\n\tmy_string last char : '{ my_string[-1] }'",
            )
 
 if __name__ == "__main__":
