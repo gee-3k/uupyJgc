78241ace7e8fcb7eab1d4fb85434e57108a7a6ae
gee3k <gee3k.dev@gmail.com>
2024-10-21 00:34:46 +0700

    upd.

--- main.py
+++ main.py
@@ -2,12 +2,17 @@
 urban university – python Junior grade course
 implemented by gee3k (c) 2024-2025
 """
-
+if __name__ == "__main__":
+    print( 'Hi, PyCharm' )
+    x = 43
+    y = 32
+    print( x * y )
+    print( "End line" )
+#
 """
 Part "Module Zero"
 have registered GitHub
 installed and tuned: PyCharm, git, py
-"""
 
 # Part to run next (homework) tasks
 import t001flint, t002vars, t003stridx
@@ -23,3 +28,4 @@ if __name__ == "__main__":
     module_1_5.start()
     module_1_6.start()
     module_1_fin.start()
+"""
