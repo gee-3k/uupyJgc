fa6838c04b4936b09c1ddb4d20107326acd98275
gee3k <gee3k.dev@gmail.com>
2024-10-25 21:59:52 +0700

    upd.

--- module_02/r05.py
+++ module_02/r05.py
@@ -24,8 +24,8 @@ def run_05():
     nm_func = stack()[ 0 ][ 3 ]
     nm_file = str( stack()[ 0 ][ 1 ] ).split( sep )[ -2: ]
     # module 02 run lesson 05
-    print( f":: { nm_file[0] }{ sep }{ nm_file[ 1 ] } { sep } func: { nm_func }",
-           "( basic operators, data types and program flow )" )
+    print( f":: { nm_file[0] }{ sep }{ nm_file[ 1 ] } { sep } func: { nm_func } —",
+           "basic operators, data types and program flow" )
     print( "\tThe goal to learn defining function and to call.\n" )
 
     r1 = get_matrix( 2, 2, 10 )
